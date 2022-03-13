5ae46eed8ec4acf61ee6e9409c3b5d99c6104315
zyx921197494 <921197494@qq.com>
2022-03-13 15:56:07 +0800

    2022年3月13日15:55:25 项目初始化

---
+++ src/main/java/com/winkel/qualityevaluation/QualityevaluationApplication.java
@@ -0,0 +1,13 @@
+package com.winkel.qualityevaluation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QualityevaluationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QualityevaluationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/winkel/qualityevaluation/QualityevaluationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.winkel.qualityevaluation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QualityevaluationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
